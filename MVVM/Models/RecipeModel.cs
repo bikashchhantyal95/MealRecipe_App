bb1586f4af11af975eb4fa4796d3cf995a23b4a6
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-09-11 22:46:15 -0400

    Add RecipeModel class
    Created folder to create MVVM structure for app. Added Recipe model class in model folder. Recipe model includes properties for recipe's ID, name, description and much more.

---
+++ MVVM/Models/RecipeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MovieRecipeMobileAPp.MVVM.Models
+{
+	public class RecipeModel
+	{
+        public int Id { get; set; } // Unique identifier for the recipe
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Instructions { get; set; }
+    }
+}
+
