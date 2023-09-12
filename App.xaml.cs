31b6170a6a65541143657f795ef5fa8aededd8d5
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-09-11 22:27:52 -0400

    Create project for the mobile recipe app

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieRecipeMobileAPp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieRecipeMobileAPp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
