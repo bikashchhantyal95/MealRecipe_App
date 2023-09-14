d10de3e42aa2ad18e34709f7cc4afe83a510c483
Bikash Chhantyal <bikashchhantyal2@gmail.com>
2023-09-14 00:38:03 -0400

    Initialize RecipeRepository
    Created a RecipeRepository class with a list of dummy data of RecipeModel instances. The GetAllRecipes method returns the list of recipes.

--- MVVM/Models/RecipeRepository.cs
+++ MVVM/Models/RecipeRepository.cs
@@ -3,7 +3,22 @@ namespace MovieRecipeMobileAPp.MVVM.Models
 {
 	public class RecipeRepository
 	{
-		
-	}
+        private List<RecipeModel> recipes;
+
+        public RecipeRepository()
+        {
+            recipes = new List<RecipeModel>
+            {
+                new RecipeModel{ Id = 1, Name= "Recipe 1", Description = "Description 1", Instructions = "Instructions 1"},
+                new RecipeModel{ Id = 2, Name= "Recipe 2", Description = "Description 2", Instructions = "Instructions 2"},
+                new RecipeModel{ Id = 3, Name= "Recipe 3", Description = "Description 3", Instructions = "Instructions 3"}
+            };
+        }
+
+        public List<RecipeModel> GetAllRecipes()
+        {
+            return recipes;
+        }
+    }
 }
 
